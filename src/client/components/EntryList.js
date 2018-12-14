0b0b0afbcb9de1870bc3ac6a94673a9b4382c2ab
Altai <alchi1206@gmail.com>
2018-12-14 13:11:27 -0500

    added search functionality

--- src/client/components/EntryList.js
+++ src/client/components/EntryList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import EntryItem from './EntryItem';
 
 class EntryList extends Component {
-  constructor(props ) {
+  constructor(props) {
     super(props);
     this.state = {
       posts: [
@@ -18,13 +18,13 @@ class EntryList extends Component {
   }
 
   render() {
-    let entries = this.state.posts.map( (entry, index) => {
-      return <EntryItem 
-        key={`entry-item-${index}`} 
-        term={entry.term} 
-        definition={entry.definition} 
-        createdBy={entry.createdBy} 
-        upvotes={entry.upvotes} 
+    let entries = this.props.entriesToRender.map((entry, index) => {
+      return <EntryItem
+        key={`entry-item-${index}`}
+        term={entry.term}
+        definition={entry.definition}
+        createdBy={entry.createdBy}
+        upvotes={entry.upvotes}
         downvotes={entry.downvotes}
       />
     });
--- src/client/components/PageContent.js
+++ src/client/components/PageContent.js
@@ -6,27 +6,53 @@ class PageContent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchValue: ''
+      searchValue: '',
+      entries: [{
+        term: 'Hola Dude',
+        definition: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        createdBy: 'Briandaman',
+        upvotes: 3,
+        downvotes: 0
+      },
+      {
+        term: 'Spin it Up',
+        definition: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        createdBy: 'Briandaman',
+        upvotes: 3,
+        downvotes: 0
+      },
+      {
+        term: 'Backpack',
+        definition: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        createdBy: 'Briandaman',
+        upvotes: 3,
+        downvotes: 0
+      }],
+      entriesToRender: []
     }
 
     this.handleSearchChange = (event) => {
       this.setState({ searchValue: event.target.value });
       console.log("handle change", this.state.searchValue)
-    }
-    this.handleSearchSubmit = (e) => {
 
-      // let entriesCopy = JSON.parse(JSON.stringify(this.state.entries))
-      let entriesCopy = [{ term: "hello" }, { term: "hey there" }, { term: "yoyoyo" }]
+
+      let entriesCopy = JSON.parse(JSON.stringify(this.state.entries))
+
       let entriesToRender = entriesCopy.filter((entry) => {
-        return entry.term.includes(this.state.searchValue)
 
+        return entry.term.toLowerCase().includes(this.state.searchValue.toLowerCase())
       })
-      this.setState({ entriesToRender: entriesToRender })
+      this.setState({ entriesToRender })
       console.log("handle submit", this.state.entriesToRender)
-
+    }
+    this.handleAllEntries = (event) => {
+      this.setState({ entriesToRender: this.state.entries })
     }
   }
 
+  componentDidUpdate() {
+    console.log("did update", this.state.entries)
+  }
   // componentWillMount() {
   //   fetch("/http://localhost:8080/search")
   //     .then((res) => {
@@ -41,13 +67,13 @@ class PageContent extends Component {
   render() {
     return (
       <section>
-        <h1>Hello Devs!</h1>
+        <h1>Urban Developtionary</h1>
         <SearchBar
           searchValue={this.state.searchValue}
           handleSearchChange={this.handleSearchChange}
-          handleSearchSubmit={this.handleSearchSubmit}
+          handleAllEntries={this.handleAllEntries}
         />
-        <EntryList />
+        <EntryList entriesToRender={this.state.entriesToRender} />
       </section>
     )
   }
