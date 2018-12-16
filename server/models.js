35a42f6c102d8b814c8a03442cb42757b08ea396
Altai <alchi1206@gmail.com>
2018-12-16 18:39:06 -0500

    added addUser/isUser controllers and routes

--- server/models.js
+++ server/models.js
@@ -10,39 +10,13 @@ const entriesSchema = new Schema({
   tags: [String]
 })
 const userSchema = new Schema({
-  username: String,
+  username: { type: String, unique: true },
   password: String,
-  entries: String,
+  entries: Array,
   badEntries: Number
 })
 
 const Users = mongoose.model('Users', userSchema);
 const Entries = mongoose.model('Entries', entriesSchema);
-let newUser = {
-  username: "Eugene",
-  password: "kittens88",
-  entries: "yada, yada, yoo",
-  badEntries: 44
-}
-let newEntry = {
-  term: "hi",
-  definition: "hi",
-  upVotes: 10,
-  downVotes: 1,
-  createdBy: "hi",
-  tags: ["hi"]
-}
-
-let entryDoc = new Entries(newEntry);
-let userDoc = new Users(newUser);
-entryDoc.save((err) => {
-  if (err) return handleError(err)
-
-})
-userDoc.save((err) => {
-  if (err) return handleError(err);
-});
-
-
 
 module.exports = Users, Entries;
