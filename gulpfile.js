5fe62e9c080eab74a49476799df23462e713d25e
bean <benesitis@gmail.com>
2019-02-04 17:10:06 -0500

    setup default gulp task to watch for client/server code changes

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+const webpackConfig = require('./webpack.config.js');
+const nodemon = require('nodemon');
+
+gulp.task('js', () => {
+  gulp
+    .src('./client/index.js')
+    .pipe(webpackStream(webpackConfig, webpack))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('serve', function() {
+  return nodemon({
+    script: 'server/server.js',
+  }).on('restart', function() {
+    console.log('restarted');
+  });
+});
+
+// Watch JS/JSX
+gulp.task('watch', function() {
+  gulp.watch('client/**/*.{js,jsx}', ['js']);
+  gulp.watch('server/**/*.{js,jsx}', ['js']);
+});
+
+gulp.task('default', ['js', 'serve', 'watch']);
--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const userController = require("./controllers/userController");
-const defController = require("./controllers/definitionController");
-const exampleController = require("./controllers/exampleController");
-const commentController = require("./controllers/commentController");
-const upvoteController = require("./controllers/UpvotesController");
-const downvoteController = require("./controllers/DownvotesController");
-const router = express.Router();         
+const userController = require('./controllers/userController');
+const defController = require('./controllers/definitionController');
+const exampleController = require('./controllers/exampleController');
+const commentController = require('./controllers/commentController');
+const upvoteController = require('./controllers/UpvotesController');
+const downvoteController = require('./controllers/DownvotesController');
+const router = express.Router();
 const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('hi');
-});
+app.use(express.static(path.join(__dirname, '../dist')));
 
 //authorization
 app.post('/api/auth', userController.isUser);
 
 //posts
-app.post('/api/definitions/',defController.addDef,exampleController.addExamples)
+app.post(
+  '/api/definitions/',
+  defController.addDef,
+  exampleController.addExamples
+);
 
-app.post('/api/comments/',commentController.addComment);
+app.post('/api/comments/', commentController.addComment);
 
-app.post('/api/upvote',upvoteController.addUpvote)
+app.post('/api/upvote', upvoteController.addUpvote);
 
-app.post('/api/upvote',upvoteController.addUpvote)
-
+app.post('/api/upvote', upvoteController.addUpvote);
 
 //gets
 app.get(
@@ -38,7 +40,8 @@ app.get(
   exampleController.getExamples,
   commentController.getComments,
   upvoteController.getUpvotes,
-  downvoteController.getDownvotes)
+  downvoteController.getDownvotes
+);
 
 app.get(
   '/api/users/:uId',
@@ -46,20 +49,19 @@ app.get(
   exampleController.getExamples,
   commentController.getComments,
   upvoteController.getUpvotes,
-  downvoteController.getDownvotes)
+  downvoteController.getDownvotes
+);
 
-app.get('api/definitions/requested', defController.getRequestedDefs)
-
+app.get('api/definitions/requested', defController.getRequestedDefs);
 
 //edits
-app.delete('/api/definitions/:dId',defController.delete)
+app.delete('/api/definitions/:dId', defController.delete);
 
-app.patch('/api/definitions/:dId',defController.update);
+app.patch('/api/definitions/:dId', defController.update);
 
-
-if(process.env.ENV_VARIABLE !== 'production'){
+if (process.env.ENV_VARIABLE !== 'production') {
   app.listen(8080);
-  console.log('listening on port 8080')
-  }
+  console.log('listening on port 8080');
+}
 
 module.exports = app;
